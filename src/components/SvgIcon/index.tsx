c7eb32a1259f653c4e4f57bf14212122b4c90d9f
Daniel Lucas <danielluccas000@gmail.com>
2023-09-26 22:43:38 -0300

    teste

--- src/components/SvgIcon/index.tsx
+++ src/components/SvgIcon/index.tsx
@@ -7,7 +7,7 @@ interface SvgIconProps {
 export function SvgIcon({ src, width, height }: SvgIconProps) {
   return (
     <img
-      src={`public/img/svg/${src}`}
+      src={`/public/assets/svg/${src}`}
       alt={src}
       width={width}
       height={height}
